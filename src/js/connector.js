e0647e7e1b7588a55609f2a737ebd76126348f15
Jon Herbert <jonherbertmusic@gmail.com>
2021-01-27 15:34:28 +0000

    simple copy

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,32 +1,22 @@
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 var onBtnClick = function (t, opts) {
-  return t.card("url").then(function (card) {
-    console.log(card);
-    var str = JSON.stringify(card, null, 2);
-    console.log(str);
-    var parsedURL = JSON.parse(str);
-    console.log(parsedURL.url);
-    copyToClipboard(parsedURL.url);
-  }).catch(error => console.log(error));
+  // return t.card("url").then(function (card) {
+  //   console.log(card);
+  //   var str = JSON.stringify(card, null, 2);
+  //   console.log(str);
+  //   var parsedURL = JSON.parse(str);
+  //   console.log(parsedURL.url);
+  copyPageUrl();
+  // }).catch(error => console.log(error));
 };
 
-function copyToClipboard(text) {
-  if (navigator.clipboard) { // default: modern asynchronous API
-    return navigator.clipboard.writeText(text);
-  } else if (window.clipboardData && window.clipboardData.setData) { // for IE11
-    window.clipboardData.setData('Text', text);
-  } else {
-    // workaround: create dummy input
-    const input = h('input', {
-      type: 'text'
-    });
-    input.value = text;
-    document.body.append(input);
-    input.focus();
-    input.select();
-    document.execCommand('copy');
-    input.remove();
+async function copyPageUrl() {
+  try {
+    await navigator.clipboard.writeText(location.href);
+    console.log('Page URL copied to clipboard');
+  } catch (err) {
+    console.error('Failed to copy: ', err);
   }
 }
 
