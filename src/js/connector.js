dfe62621fef66d5bfec64d96c9f40124bcb582b6
Jon Herbert <jonherbertmusic@gmail.com>
2021-01-27 13:57:05 +0000

    change to clipboard-polyfill

--- src/js/connector.js
+++ src/js/connector.js
@@ -1,14 +1,22 @@
+import * as clipboard from "clipboard-polyfill/text";
+
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
 var onBtnClick = function (t, opts) {
-  var copy = require('clipboard-copy');
   return t.card("url").then(function (card) {
     console.log(card);
     var str = JSON.stringify(card, null, 2);
     console.log(str);
     var parsedURL = JSON.parse(str);
     console.log(parsedURL.url);
-    copy(parsedURL.url);
+    clipboard.writeText(parsedURL.url).then(
+      function () {
+        console.log("success!");
+      },
+      function () {
+        console.log("error!");
+      }
+    );
   });
 };
 
