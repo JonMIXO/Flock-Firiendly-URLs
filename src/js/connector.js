b5d4fe154568cc3346f7c21b730ed1aec2956695
Jon Herbert <jonherbertmusic@gmail.com>
2021-01-27 12:54:07 +0000

    added logs

--- src/js/connector.js
+++ src/js/connector.js
@@ -3,6 +3,8 @@ var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4b
 var onBtnClick = function (t, opts) {
   return t.card("url").then(function (card) {
     var url = JSON.stringify(card, null, 2);
+    console.log(url);
+    console.log(url.value);
     navigator.clipboard.writeText(url.value).then(function (c) {
       console.log(c);
     }, function (err) {
